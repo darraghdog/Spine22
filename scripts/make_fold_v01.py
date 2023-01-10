9e716f7f0a0b7d962223e953cfb49ed9e4d7801f
darraghdog <darragh.hanley@gmail.com>
2023-01-10 23:02:40 +0000

    .

--- scripts/make_fold_v01.py
+++ scripts/make_fold_v01.py
@@ -24,7 +24,7 @@ trndf = pd.read_csv(trnfile)
 # Remove toronto data
 utfile = 'datamount/RSNA_Kaggle_Experiment.csv'
 utdf = pd.read_csv(utfile)
-trndf = trndf.loc[~trndf['StudyInstanceUID'].isin(utdf.StudyInstanceUID)].reset_index(drop = True)
+trndf = trndf.loc[trndf['StudyInstanceUID'].isin(utdf.StudyInstanceUID)].reset_index(drop = True)
 
 
 k_fold = IterativeStratification(n_splits=5, order=2)
@@ -40,46 +40,17 @@ print(trndf.groupby(trndf.fold)[cols].agg('mean'))
 print(trndf.shape)
 trndf.to_csv('datamount/train_folded_v01.csv', index = False)
 
-
 '''
       patient_overall    C1    C2    C3    C4    C5    C6    C7
 fold
-0               0.473 0.072 0.141 0.037 0.054 0.082 0.139 0.193
-1               0.493 0.072 0.141 0.037 0.052 0.079 0.136 0.196
-2               0.469 0.072 0.141 0.035 0.052 0.079 0.136 0.196
-3               0.460 0.074 0.141 0.037 0.054 0.079 0.139 0.193
-4               0.485 0.072 0.141 0.035 0.054 0.082 0.136 0.196
-'''
-
-
-
-import os
-import sys
-import platform
-import pandas as pd
-from utils import set_pandas_display
-sys.path.append("configs")
-sys.path.append("models")
-sys.path.append("data")
-sys.path.append("postprocess")
-
-# PATH = '/mount/Spine22'
-trnfile = 'datamount/train.csv'
-utfile = 'datamount/RSNA_Kaggle_Experiment.csv'
-trndf = pd.read_csv(trnfile)
-utdf = pd.read_csv(utfile)
-
-trndf['fold'] = 0 # Validation data
-trndf.loc[trndf['StudyInstanceUID'].isin(utdf.StudyInstanceUID), 'fold'] =  1
-
-
-trndf.to_csv('datamount/train_folded_v01.csv', index = False)
-
-print(trndf['fold'].value_counts())
+0               0.469 0.073 0.141 0.032 0.053 0.076 0.129 0.191
+1               0.466 0.076 0.141 0.035 0.053 0.073 0.132 0.188
+2               0.469 0.073 0.144 0.035 0.053 0.076 0.129 0.188
+3               0.472 0.076 0.144 0.032 0.053 0.076 0.132 0.191
+4               0.478 0.076 0.141 0.035 0.050 0.073 0.132 0.191
+(1705, 10)
 
 '''
-1    1705 (Training data)
-0     314 (Validation data)
-'''
+
 
 
