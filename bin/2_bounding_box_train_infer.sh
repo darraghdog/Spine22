1fb5abd647bf95df533052402242f12f04b488bd
darraghdog <darragh.hanley@gmail.com>
2023-02-02 14:51:40 +0000

    .

--- bin/2_bounding_box_train_infer.sh
+++ bin/2_bounding_box_train_infer.sh
@@ -17,7 +17,7 @@ for FOLD in 0 1 2 3 4 # -1
 do
     search_dir='weights/cfg_loc_dh_01B/fold'
     search_dir+=$FOLD
-    for WEIGHTS_NAME in "$search_dir"/*
+    for WEIGHTS_NAME in "$search_dir"/checkpoint*
     do
         echo "Running inference on "$WEIGHTS_NAME
         python train.py -C cfg_loc_dh_01B_test --fold $FOLD --pretrained_weights WEIGHTS_NAME
