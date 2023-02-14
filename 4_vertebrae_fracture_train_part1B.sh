32ec33f808dbe3a111e7514fac99e76c985c7ad0
darraghdog <darragh.hanley@gmail.com>
2023-02-14 11:29:47 +0000

    .

---
+++ 4_vertebrae_fracture_train_part1B.sh
@@ -0,0 +1,14 @@
+# PART 1
+# Train the initial model on slice level vertebrae and fracture labels
+# Runtime approximately 10 hours for each fold - so total 100 hours
+```
+for FOLD in -1 -1 -1 -1 # 0
+do
+    python train.py -C cfg_dh_fracseq_04F_crop --fold $FOLD
+done
+```
+for FOLD in -1 -1 -1 # 0
+do
+    python train.py -C cfg_dh_fracseq_04G_crop --fold $FOLD
+done
+
